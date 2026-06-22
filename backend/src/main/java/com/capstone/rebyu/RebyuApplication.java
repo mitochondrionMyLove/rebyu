27831fbcf2e5454658d97a725338af8ddc16d3a3
galazel <glyzelgalagar123@gmail.com>
2026-06-22 21:44:28 +0800

    initial commit

---
+++ backend/src/main/java/com/capstone/rebyu/RebyuApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.rebyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebyuApplication.class, args);
+	}
+
+}
