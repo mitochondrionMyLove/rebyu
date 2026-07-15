ffc90ad9e788713333193c8a514982a0c2387f19
Glyzel Galagar <133832079+galazel@users.noreply.github.com>
2026-07-15 13:08:55 +0800

    Update CorsConfig.java

--- backend-java/src/main/java/com/capstone/rebyu/config/CorsConfig.java
+++ backend-java/src/main/java/com/capstone/rebyu/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173") );
+        configuration.setAllowedOrigins(List.of("https://rebyu.up.railway.app/") );
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
